269c3d1f87315803a54f26fda99d582f80cbed11
Lance Gleason <lgleasain@yahoo.com>
2012-11-04 17:57:15 -0500

    fixing travis build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,25 +4,4 @@ require 'json'
 require 'simplecov'
 SimpleCov.start
 
-# stubbing out rails generators.
-module Rails
-  module Generators
-    class Base
-      def self.source_root arg
-      end
-
-      def self.argument arg, args
-      end
-
-      def self.desc arg
-      end
-    end
-  end
-end
-
-module Passbook
-  def configure
-  end
-end
-
-Dir['lib/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
+Dir['lib/passbook/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
