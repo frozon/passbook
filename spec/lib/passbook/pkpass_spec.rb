bc17944c1cea4a2385b0cf3d18e1416a757de2e3
Lance Gleason <lgleasain@yahoo.com>
2012-11-04 16:33:30 -0500

    refactoring the specs to something that makes sense

--- spec/lib/passbook/pkpass_spec.rb
+++ spec/lib/passbook/pkpass_spec.rb
@@ -10,77 +10,89 @@ describe Passbook  do
       pass.p12_cert = ENV['P12_PATH']
       pass.p12_password = ENV['PASSBOOK_P12_PASSWORD']
     end
-    @content = {
-                formatVersion: 1,
-                passTypeIdentifier: "pass.passbook.test",
-                serialNumber: "001",
-                teamIdentifier: ENV['APPLE_TEAM_ID'],
-                relevantDate: "2012-10-02",
-                locations: [  #TODO
-                  {
-                    longitude: 2.35403,
-                    latitude: 48.893855
-                  }
-                ],
-                organizationName: "WorldCo",
-                description: "description",
-                foregroundColor: "rgb(227,210,18)",
-                backgroundColor: "rgb(60, 65, 76)",
-                logoText: "Event",
-                eventTicket: {
-                  primaryFields: [
-                    {
-                      key: "date",
-                      label: "DATE",
-                      value: "date"
-                    }
-                  ],
-                  backFields: [
-                    {
-                      key: "description",
-                      label: "DESCRIPTION",
-                      value: "description"
-                    },
-                    {
-                      key: "aboutUs",
-                      label: "MORE",
-                      value: "about us"
-                    }
-                  ]
-                }
-              }
-    @pass = Passbook::PKPass.new @content.to_json
-  end
-  
-  it "should create a zip file by default" do
-    base_path = "spec/data"
-    @pass.addFiles ["#{base_path}/icon.png","#{base_path}/icon@2x.png","#{base_path}/logo.png","#{base_path}/logo@2x.png"]
-    entries = ["pass.json", "manifest.json", "signature", "icon.png", "icon@2x.png", "logo.png", "logo@2x.png"]
-    zip_path = @pass.file
-    #re-reading zip to see if correctly formed
-    Zip::ZipInputStream::open(zip_path) {|io|
-      while (entry = io.get_next_entry)
-        entries.should include(entry.name)
-      end
-    }
   end
 
-  it "should create a stringIO output if selected" do
-    base_path = "spec/data"
-    @pass.addFiles ["#{base_path}/icon.png","#{base_path}/icon@2x.png","#{base_path}/logo.png","#{base_path}/logo@2x.png"]
-    entries = ["pass.json", "manifest.json", "signature", "icon.png", "icon@2x.png", "logo.png", "logo@2x.png"]
-    zip_out = @pass.stream
-    zip_out.class.should eq(Class::StringIO)
-    #creating file, re-reading zip to see if correctly formed
-    t = Tempfile.new("pass.pkpass")
-    t.write zip_out.string
-    t.close
-    Zip::ZipInputStream::open(t.path) {|io|
-      while (entry = io.get_next_entry)
-        entries.should include(entry.name)
-      end
-    }
-    t.delete
-  end 
+  let (:content) {{
+    formatVersion: 1,
+    passTypeIdentifier: "pass.passbook.test",
+    serialNumber: "001",
+    teamIdentifier: ENV['APPLE_TEAM_ID'],
+    relevantDate: "2012-10-02",
+    locations: [  #TODO
+      {
+    longitude: 2.35403,
+    latitude: 48.893855
+  }
+  ],
+    organizationName: "WorldCo",
+    description: "description",
+    foregroundColor: "rgb(227,210,18)",
+    backgroundColor: "rgb(60, 65, 76)",
+    logoText: "Event",
+    eventTicket: {
+    primaryFields: [
+      {
+    key: "date",
+    label: "DATE",
+    value: "date"
+  }
+  ],
+    backFields: [
+      {
+    key: "description",
+    label: "DESCRIPTION",
+    value: "description"
+  },
+    {
+    key: "aboutUs",
+    label: "MORE",
+    value: "about us"
+  }
+  ]
+  }
+  }}
 
+  let (:pass) {Passbook::PKPass.new content.to_json}
+
+  context 'outputs' do
+    let (:base_path) {'spec/data'}
+    let (:entries) {["pass.json", "manifest.json", "signature", "icon.png", "icon@2x.png", "logo.png", "logo@2x.png"]}
+
+    before :each do
+      pass.addFiles ["#{base_path}/icon.png","#{base_path}/icon@2x.png","#{base_path}/logo.png","#{base_path}/logo@2x.png"]
+      pass.should_receive(:createSignature).and_return('Signed by the Honey Badger')
+      @file_entries = []
+      Zip::ZipInputStream::open(zip_path) {|io|
+        while (entry = io.get_next_entry)
+          @file_entries << entry.name
+        end
+      }
+    end
+
+    context 'zip file' do
+      let(:zip_path) {pass.file}
+
+      subject {entries}
+      it {should eq @file_entries}
+    end
+
+    context 'StringIO' do
+      let (:temp_file) {Tempfile.new("pass.pkpass")}
+      let (:zip_path) {        
+        zip_out = pass.stream
+        zip_out.class.should eq(Class::StringIO)
+        #creating file, re-reading zip to see if correctly formed
+        temp_file.write zip_out.string
+        temp_file.close
+        temp_file.path
+      }
+
+      subject {entries}
+      it {should eq @file_entries}
+
+      after do
+        temp_file.delete
+      end 
+    end
+  end
 end
