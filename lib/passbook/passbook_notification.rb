0caec7bffe3f175ec6d40b88262859e5f41c1d07
Lance Gleason <lgleasain@yahoo.com>
2013-01-04 18:22:22 -0500

    refactoring to support passbook notification object in grocer and getting rid of ugly monkey patch

--- lib/passbook/passbook_notification.rb
+++ lib/passbook/passbook_notification.rb
@@ -1,12 +1,8 @@
 module Passbook
-  class PushNotification
+  class PassbookNotification
     def self.send_notification(device_token)
       pusher = Grocer.pusher({:certificate => Passbook.notification_cert, :gateway => Passbook.notification_gateway})
       notification = Grocer::Notification.new(:device_token => device_token)
-
-      def notification.validate_payload
-      end
-
       pusher.push notification
     end
   end
