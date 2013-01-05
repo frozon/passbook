c1a24279c61e7e96debf591ef4747e0bc8dded7a
Lance Gleason <lgleasain@yahoo.com>
2013-01-04 21:01:24 -0500

    fixing issue with include

--- lib/passbook.rb
+++ lib/passbook.rb
@@ -1,7 +1,7 @@
 require "passbook/version"
 require "passbook/pkpass"
 require 'active_support/core_ext/module/attribute_accessors'
-require 'passbook/push_notification'
+require 'passbook/passbook_notification'
 
 module Passbook
   mattr_accessor :p12_cert, :p12_password, :wwdc_cert, :p12_certificate, :p12_key, :notification_cert, :notification_gateway
