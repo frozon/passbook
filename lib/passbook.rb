4b7affa5ab9d304785a417787650d193d399d593
Lance Gleason <lgleasain@yahoo.com>
2013-01-04 23:40:49 -0500

    adding the correct grocer calls to get rid of the ugly monkey patch

--- lib/passbook.rb
+++ lib/passbook.rb
@@ -2,6 +2,7 @@ require "passbook/version"
 require "passbook/pkpass"
 require 'active_support/core_ext/module/attribute_accessors'
 require 'passbook/push_notification'
+require 'grocer/passbook_notification'
 
 module Passbook
   mattr_accessor :p12_cert, :p12_password, :wwdc_cert, :p12_certificate, :p12_key, :notification_cert, :notification_gateway
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler'
 require 'json'
 require 'simplecov'
+require 'grocer/passbook_notification'
 SimpleCov.start
 
 Dir['lib/passbook/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
